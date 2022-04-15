cab207f7ccc6ba8542f4dd91277ace70507e1601
TOMO-YOSHI <lucky.paddy10@gmail.com>
2022-04-15 00:52:59 -0700

    complete reaction-timer project

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,12 +1,44 @@
 <template>
-  <div class="block">
+  <div class="block" v-if="showBlock" @click="stopTimer">
       click me
   </div>
 </template>
 
 <script>
 export default {
-    props: ['delay']
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        console.log("component mounted")
+        setTimeout(()=>{
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay)
+    },
+    updated() {
+        console.log("component updated")
+    },
+    unmounted() {
+        console.log('unmounted')
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10);
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+            this.$emit('end', this.reactionTime)
+        }
+    }
 }
 </script>
 
